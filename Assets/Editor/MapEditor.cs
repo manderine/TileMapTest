a2b69aa4f8a9c28836a1e3c1b929f3a7bb6f9755
manderine <lmj7763@korea.com>
2017-07-14 09:28:23 +0900

    Upload

---
+++ Assets/Editor/MapEditor.cs
@@ -0,0 +1,171 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+using System.Collections.Generic;
+
+[CustomEditor(typeof(Map))]
+public class MapEditor : Editor 
+{
+	GameObject[] prefabs;
+	GameObject selectedPrefab;
+	List<GameObject> spawnedGO = new List<GameObject>();
+
+	public override void OnInspectorGUI()
+	{
+		DrawDefaultInspector ();
+
+		//Load all prefabs as objects from the 'Prefabs' folder
+		Object[] obj =  Resources.LoadAll ("Prefabs",typeof(GameObject));
+
+		//initialize the game object array
+		prefabs = new GameObject[obj.Length];
+
+		//store the game objects in the array
+		for(int i=0;i<obj.Length;i++)
+		{
+			prefabs[i]=(GameObject)obj[i];
+		}
+
+		GUILayout.BeginHorizontal ();
+
+        if(prefabs!=null)
+        {
+            int elementsInThisRow=0;
+            for( int i=0; i<prefabs.Length; i++)
+            {
+                elementsInThisRow++;
+
+                //get the texture from the prefabs
+                Texture prefabTexture = prefabs[i].GetComponent<SpriteRenderer> ().sprite.texture;
+
+                //create one button for earch prefabs 
+                //if a button is clicked, select that prefab and focus on the scene view
+                if(GUILayout.Button(prefabTexture,GUILayout.MaxWidth(50), GUILayout.MaxHeight(50)))
+                {    
+                    selectedPrefab = prefabs[i];
+                    EditorWindow.FocusWindowIfItsOpen<SceneView>();
+
+                }
+
+                //move to next row after creating a certain number of buttons so it doesn't overflow horizontally
+                if(elementsInThisRow>Screen.width/70)
+                {
+                    elementsInThisRow=0;
+                    GUILayout.EndHorizontal();
+                    GUILayout.BeginHorizontal ();        
+                }
+            }
+        }
+        GUILayout.EndHorizontal();
+
+	}
+
+	void OnSceneGUI()
+	{
+		Handles.BeginGUI();
+		GUILayout.Box("Map Edit Mode");
+		if(selectedPrefab==null)
+		{
+			GUILayout.Box("No prefab selected!");
+ 		}
+		Handles.EndGUI();     
+
+		Vector3	spawnPosition = HandleUtility.GUIPointToWorldRay (Event.current.mousePosition).origin;
+
+	    //if 'E' pressed, spawn the selected prefab
+        if (Event.current.type==EventType.KeyDown &&Event.current.keyCode == KeyCode.H) 
+        {
+            
+            TileMap tm = FindObjectOfType<TileMap>();
+			if( tm != null ) {
+				tm.SetTile( 0, 0, 1 );
+			}
+        }
+
+	    //if 'E' pressed, spawn the selected prefab
+        if (Event.current.type==EventType.KeyDown &&Event.current.keyCode == KeyCode.E) 
+        {
+            
+            Spawn(spawnPosition);
+        }
+
+
+		//if 'X' is pressed, undo (remove the last spawned prefab)
+		if (Event.current.type==EventType.KeyDown &&Event.current.keyCode == KeyCode.X) 
+		{	
+			if(spawnedGO.Count>0)
+			{
+				DestroyImmediate(spawnedGO[spawnedGO.Count-1]);
+				spawnedGO.RemoveAt(spawnedGO.Count-1);
+			}
+		}
+
+		if (Event.current.type==EventType.KeyDown &&Event.current.keyCode == KeyCode.R) 
+    	{
+    		
+    		Vector2 mouseWorldPosition = new Vector2(spawnPosition.x, spawnPosition.y);
+			RaycastHit2D hitInfo = Physics2D.Raycast(mouseWorldPosition, Vector2.zero);
+ 
+			if(hitInfo.collider != null)
+			{
+			    selectedGameObject = hitInfo.collider.gameObject;
+			}
+         }
+
+		if(selectedGameObject!=null)
+        {
+            Handles.Label(selectedGameObject.transform.position, "X");
+
+        }
+
+
+		//used to indicate the exact point where the prefab will be instantiated
+		Handles.CircleCap (0, spawnPosition, Quaternion.Euler(0,0,0), .05f);
+
+		if(selectedPrefab!=null)
+		{
+			if(selectedGameObject!=null)
+			{
+				float selectedGameObjectWidth = selectedGameObject.GetComponent<SpriteRenderer>().bounds.size.x;
+				float selectedGameObjectHeight = selectedGameObject.GetComponent<SpriteRenderer>().bounds.size.y;
+
+				float selectedPrefabWidth = selectedPrefab.GetComponent<SpriteRenderer>().bounds.size.x;
+				float selectedPrefabHeight = selectedPrefab.GetComponent<SpriteRenderer>().bounds.size.y;
+
+				if (Event.current.type==EventType.KeyDown &&Event.current.keyCode == KeyCode.W) 
+				{
+					spawnPosition = new Vector3(selectedGameObject.transform.position.x, selectedGameObject.transform.position.y+(selectedGameObjectHeight/2)+(selectedPrefabHeight/2), 0);
+					Spawn(spawnPosition);
+				}
+				if (Event.current.type==EventType.KeyDown &&Event.current.keyCode == KeyCode.S) 
+				{
+					spawnPosition = new Vector3(selectedGameObject.transform.position.x, selectedGameObject.transform.position.y-(selectedGameObjectHeight/2)-(selectedPrefabHeight/2), 0);
+					Spawn(spawnPosition);
+				}
+				if (Event.current.type==EventType.KeyDown &&Event.current.keyCode == KeyCode.A) 
+				{
+					spawnPosition = new Vector3(selectedGameObject.transform.position.x-(selectedGameObjectWidth/2)-(selectedPrefabWidth/2), selectedGameObject.transform.position.y, 0);
+					Spawn(spawnPosition);
+				}
+				if (Event.current.type==EventType.KeyDown &&Event.current.keyCode == KeyCode.D) 
+				{
+					spawnPosition = new Vector3(selectedGameObject.transform.position.x+(selectedGameObjectWidth/2)+(selectedPrefabWidth/2), selectedGameObject.transform.position.y, 0);
+					Spawn(spawnPosition);
+				}
+	
+			}
+		}
+
+		SceneView.RepaintAll();
+	}
+
+	GameObject selectedGameObject;
+	void Spawn(Vector2 _spawnPosition)
+    {
+        GameObject go = (GameObject)Instantiate(selectedPrefab,new Vector3(_spawnPosition.x, _spawnPosition.y, 0), selectedPrefab.transform.rotation);
+        selectedGameObject = go;
+        go.name = selectedPrefab.name;
+        spawnedGO.Add(go);
+    }
+
+}
---
+++ Assets/TileMap.cs
@@ -0,0 +1,274 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public enum ANCHOR_TYPE {
+	RIGHT_UP = 0,
+	RIGHT_DOWN,
+	LEFT_UP,
+	LEFT_DOWN
+}
+
+[ExecuteInEditMode]
+[RequireComponent(typeof(MeshFilter))]
+[RequireComponent(typeof(MeshRenderer))]
+public class TileMap : MonoBehaviour {
+	public ANCHOR_TYPE _anchor_type = ANCHOR_TYPE.LEFT_DOWN;
+
+	public int _sprite_size = 100;
+	public float _tile_scale = 0.1f;
+
+	public int _tile_count_x = 1;
+	public int _tile_count_y = 1;
+
+	public Texture2D _terrainTiles;
+	public List<int> _ids = new List<int>();
+
+		ANCHOR_TYPE __anchor_type = ANCHOR_TYPE.LEFT_DOWN;
+
+		int __sprite_size = 0;
+		float __tile_scale = 0;
+
+		int __tile_count_x = 0;
+		int __tile_count_y = 0;
+
+		Texture2D __terrainTiles;
+		int [] __ids = null;
+
+	private Rect[] _tiles;
+	private Mesh _mesh;
+
+	private MeshFilter _mesh_filter;
+
+	// Use this for initialization
+	void Start () {
+		_mesh_filter = GetComponent<MeshFilter>();
+
+		LateUpdate();
+	}
+
+	public bool isEqual() {
+		if( _anchor_type != __anchor_type ) {
+			return false;
+		}
+
+		if( _tile_scale != __tile_scale ) {
+			return false;
+		}
+
+		if( _terrainTiles != __terrainTiles ) {
+			return false;
+		}
+
+		int [] ids1_arr = { _sprite_size, _tile_count_x, _tile_count_y };
+		List<int> ids1_list = new List<int>( ids1_arr );
+		if( (_ids != null) && (_ids.Count > 0) ) {
+			ids1_list.AddRange( _ids );
+		}
+
+		int [] ids2_arr = { __sprite_size, __tile_count_x, __tile_count_y };
+		List<int> ids2_list = new List<int>( ids2_arr );
+		if( (__ids != null) && (__ids.Length > 0) ) {
+			ids2_list.AddRange( __ids );
+		}
+
+		if( ids1_list.Count != ids2_list.Count ) {
+			return false;
+		}
+
+		for( int i=0; i<ids1_list.Count; i++ ) {
+			if( ids1_list[i] != ids2_list[i] ) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+	public void SetEqual() {
+		__anchor_type = _anchor_type;
+
+		__sprite_size = _sprite_size;
+		__tile_scale = _tile_scale;
+
+		__tile_count_x = _tile_count_x;
+		__tile_count_y = _tile_count_y;
+
+		__terrainTiles = _terrainTiles;
+
+		__ids = _ids.ToArray();
+	}
+
+	private void LateUpdate() {
+		if( isEqual() == false ) {
+			SetEqual();
+
+			BuildMesh();
+		}
+	}
+
+	private void OnDestroy() {
+		_tiles = null;
+
+		if( _mesh != null ) {
+			_mesh.Clear();
+			_mesh = null;
+		}
+
+		_mesh_filter.sharedMesh = null;
+	}
+
+	int GetIndex( int x, int y ) {
+		return ((_tile_count_y - 1 - y) * _tile_count_x + x);
+	}
+
+	int GetIndexToID( int index ) {
+		if( (index >= 0) && (index < _ids.Count) ) {
+			return _ids[ index ];
+		}
+		return -1;
+	}
+
+	bool GetTile( int id, out Rect tile ) {
+		tile = new Rect();
+		if( (id >= 0) && (id < _tiles.Length) ) {
+			tile = _tiles[ id ];
+			return true;
+		}
+		return false;
+	}
+
+	Rect [] GetTiles() {
+		int count_x = (_terrainTiles.width + _sprite_size - 1) / _sprite_size;
+		int count_y = (_terrainTiles.height + _sprite_size - 1) / _sprite_size;
+
+		float x_offset = (float) _sprite_size / _terrainTiles.width;
+		float y_offset = (float) _sprite_size / _terrainTiles.height;
+
+		Rect [] tiles = new Rect [ count_x * count_y ];
+
+		int index;
+		for( int y=0; y<count_y; y++ ) {
+			for( int x=0; x<count_x; x++ ) {
+				index = (count_y - 1 - y) * count_x + x;
+				tiles[ index ] = new Rect( x_offset * x, y_offset * y, x_offset, y_offset );
+			}
+		}
+
+		return tiles;
+	}
+
+	public void SetTile( int x, int y, int id_new ) {
+		int index = GetIndex( x, y );
+
+		int id = GetIndexToID( index );
+		if( id == id_new ) {
+			return;
+		}
+
+		int tileIndex = 4 * index;
+		Vector2 [] uv = _mesh_filter.sharedMesh.uv;
+
+		Rect tile;
+		if( GetTile( id_new, out tile ) == true ) {
+			uv[ tileIndex + 0 ] = new Vector2( tile.x, tile.y );
+			uv[ tileIndex + 1 ] = new Vector2( tile.x + tile.width, tile.y );
+			uv[ tileIndex + 2 ] = new Vector2( tile.x + tile.width, tile.y + tile.height );
+			uv[ tileIndex + 3 ] = new Vector2( tile.x, tile.y + tile.height );
+		} else {
+			uv[ tileIndex + 0 ] = Vector2.zero;
+			uv[ tileIndex + 1 ] = Vector2.zero;
+			uv[ tileIndex + 2 ] = Vector2.zero;
+			uv[ tileIndex + 3 ] = Vector2.zero;
+		}
+
+		_mesh_filter.sharedMesh.uv = uv;
+	}
+
+	public void BuildMesh() {
+		OnDestroy();
+
+		if( (_terrainTiles == null) ||
+			(_sprite_size == 0) || (_tile_scale == 0) ||
+			(_tile_count_x == 0) || (_tile_count_y == 0) ) {
+			return;
+		}
+
+		int numTiles = _tile_count_x * _tile_count_y;
+		int numTriangles = numTiles * 6;
+		int numVertices = numTiles * 4;
+
+		_tiles = GetTiles();
+
+		// Generate the mesh data
+		Vector3 [] vertices = new Vector3[ numVertices ];
+		Vector3 [] normals = new Vector3[ numVertices ];
+		Vector2 [] uv = new Vector2[ numVertices ];
+		int [] triangles = new int [ numTriangles ];
+
+		float offset = _sprite_size * _tile_scale;
+		
+		for( int y=0; y<_tile_count_y; y++ ) {
+			for( int x=0; x<_tile_count_x; x++ ) {
+				float _x = (float)x * offset;
+				float _y = (float)y * offset;
+
+				int index = GetIndex( x, y );
+				int id = GetIndexToID( index );
+
+				Rect tile;
+				if( GetTile( id, out tile ) == false ) {
+					continue;
+				}
+
+				int tileIndex = 4 * index;
+
+				vertices[ tileIndex + 0 ] = new Vector3( _x, _y );
+				vertices[ tileIndex + 1 ] = new Vector3( _x + offset, _y );
+				vertices[ tileIndex + 2 ] = new Vector3( _x + offset, _y + offset );
+				vertices[ tileIndex + 3 ] = new Vector3( _x, _y + offset );
+
+				uv[ tileIndex + 0 ] = new Vector2( tile.x, tile.y );
+				uv[ tileIndex + 1 ] = new Vector2( tile.x + tile.width, tile.y );
+				uv[ tileIndex + 2 ] = new Vector2( tile.x + tile.width, tile.y + tile.height );
+				uv[ tileIndex + 3 ] = new Vector2( tile.x, tile.y + tile.height );
+
+				normals[ tileIndex + 0 ] = new Vector3( 0, 0, -1 );
+				normals[ tileIndex + 1 ] = new Vector3( 0, 0, -1 );
+				normals[ tileIndex + 2 ] = new Vector3( 0, 0, -1 );
+				normals[ tileIndex + 3 ] = new Vector3( 0, 0, -1 );
+
+				int triangleIndex = 6 * index;
+
+				triangles[triangleIndex + 0] = tileIndex;
+				triangles[triangleIndex + 1] = tileIndex + 2;
+				triangles[triangleIndex + 2] = tileIndex + 1;
+
+				triangles[triangleIndex + 3] = tileIndex;
+				triangles[triangleIndex + 4] = tileIndex + 3;
+				triangles[triangleIndex + 5] = tileIndex + 2;
+
+			}
+		}
+
+		// Create a new Mesh and populate with the data
+		if( _mesh == null ) {
+			_mesh = new Mesh();
+		}
+
+		_mesh.name = "tile_map";
+		_mesh.vertices = vertices;
+		_mesh.normals = normals;
+		_mesh.uv = uv;
+		_mesh.triangles = triangles;
+
+		_mesh_filter.sharedMesh = _mesh;
+		
+		MeshRenderer mesh_renderer = GetComponent<MeshRenderer>();
+		if( (mesh_renderer.sharedMaterials != null) && (mesh_renderer.sharedMaterials.Length > 0) && (mesh_renderer.sharedMaterials[0] != null) ) {
+			mesh_renderer.sharedMaterials[0].mainTexture = _terrainTiles;
+		}
+
+		Debug.Log( "Done Mesh!" );
+	}
+}
